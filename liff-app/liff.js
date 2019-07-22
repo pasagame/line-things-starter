361adf83e6c3832b3ca8d7d32a66810c336718be
pasagame <pasagame@gmail.com>
2019-07-22 10:44:54 +0700

    Update liff.js

--- liff-app/liff.js
+++ liff-app/liff.js
@@ -1,5 +1,5 @@
 // User service UUID: Change this to your generated service UUID
-const USER_SERVICE_UUID         = '819bd0ab-c427-4ffb-b00f-58a05b31908c'; // LED, Button
+const USER_SERVICE_UUID         = '71e82300-9dfa-4a3d-9b21-53452d1a2c34'; // LED, Button
 // User service characteristics
 const LED_CHARACTERISTIC_UUID   = 'E9062E71-9E62-4BC6-B0D3-35CDCD9B027B';
 const BTN_CHARACTERISTIC_UUID   = '62FBD229-6EDD-4D1A-B554-5C4E1BB29169';
